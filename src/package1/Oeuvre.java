21927de40523bca36533e886602168bb4e98e269
DanyKuoy <danykuoy@gmail.com>
2023-03-22 17:43:49 +0100

    Initial commit

---
+++ src/package1/Oeuvre.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Oeuvre {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int calcul = 8;
+        System.out.println(calcul);
+    }
+}
