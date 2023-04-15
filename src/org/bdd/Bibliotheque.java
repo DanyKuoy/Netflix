6e2143f9ca0d7558be4e363f11defea0df129b2f
gendrecharlotte <charlotte.gendre@icloud.com>
2023-04-15 21:54:21 +0200

    bibliotheque : ajout de fonctionnalite

--- src/org/bdd/Bibliotheque.java
+++ src/org/bdd/Bibliotheque.java
@@ -116,4 +116,122 @@ public class Bibliotheque {
 
         return listeRecherche;
     }
+
+    /* Méthode permettant de réaliser une recherche d'oeuvre par son titre */
+    public boolean rechercheTitre(String nom, ArrayList<Oeuvre> listeRecherche) {
+        /// Déclaration de variable
+        int nbOeuvre = 0;
+
+        /// Recherche dans la liste des films
+        for(int i=0; i<this.getListeFilm().size(); i++) {
+            if(this.getListeFilm().get(i).getNomOeuvre().toLowerCase().contains(nom.toLowerCase())) {
+                listeRecherche.add(this.getListeFilm().get(i));
+                nbOeuvre++;
+            }
+        }
+
+        /// Recherche dans la liste des series
+        for(int j=0; j<this.getListeSerie().size(); j++) {
+            if(this.getListeSerie().get(j).getNomOeuvre().toLowerCase().contains(nom.toLowerCase())) {
+                listeRecherche.add(this.getListeSerie().get(j));
+                nbOeuvre++;
+            }
+        }
+
+        /// Retourner un booléen selon le nombre d'oeuvres trouvé
+        if(nbOeuvre==0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /* Méthode permettant de réaliser une recherche d'oeuvre par son réalisateur*/
+    public boolean rechercheRealisateur(String nom, ArrayList<Oeuvre> listeRecherche) {
+        /// Déclaration de variable
+        int nbOeuvre = 0;
+
+        /// Recherche dans la liste des films
+        for(int i=0; i<this.getListeFilm().size(); i++) {
+            if(this.getListeFilm().get(i).getRealisateurOeuvre().toLowerCase().contains(nom.toLowerCase())) {
+                listeRecherche.add(this.getListeFilm().get(i));
+                nbOeuvre++;
+            }
+        }
+
+        /// Recherche dans la liste des series
+        for(int j=0; j<this.getListeSerie().size(); j++) {
+            if(this.getListeSerie().get(j).getRealisateurOeuvre().contains(nom.toLowerCase())) {
+                listeRecherche.add(this.getListeSerie().get(j));
+                nbOeuvre++;
+            }
+        }
+
+        /// Retourner un booléen selon le nombre d'oeuvres trouvé
+        if(nbOeuvre==0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /* Méthode permettant de réaliser une recherche d'oeuvre selon un acteur/une actrice*/
+    public boolean rechercheActeur(String nom, ArrayList<Oeuvre> listeRecherche) {
+        /// Déclaration de variable
+        int nbOeuvre = 0;
+
+        /// Recherche dans la liste des films
+        for(int i=0; i<this.getListeFilm().size(); i++) {
+            /// Récupération des acteurs/actrices du film
+            String[] distribution1 = this.getListeFilm().get(i).getDistributionOeuvre().split(", ");
+            for(String acteur: distribution1) {
+                if(acteur.toLowerCase().contains(nom.toLowerCase())) {
+                    listeRecherche.add(this.getListeFilm().get(i));
+                    nbOeuvre++;
+                }
+            }
+        }
+
+        /// Recherche dans la liste des series
+        for(int j=0; j<this.getListeSerie().size(); j++) {
+            /// Récupération des acteurs/actrices du film
+            String[] distribution2 = this.getListeSerie().get(j).getDistributionOeuvre().split(", ");
+            for(String acteur: distribution2) {
+                if(acteur.toLowerCase().contains(nom.toLowerCase())) {
+                    listeRecherche.add(this.getListeSerie().get(j));
+                    nbOeuvre++;
+                }
+            }
+        }
+
+        /// Retourner un booléen selon le nombre d'oeuvres trouvé
+        if(nbOeuvre==0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /* Méthode permettant de rechercher des oeuvres selon un genre donné */
+    public boolean rechercheGenre(String nom, ArrayList<Oeuvre> listeRecherche) {
+        /// Déclaration de variable
+        int nbOeuvre = 0;
+
+        /// Recherche dans la map des films
+
+
+        /// Recherche dans la map des series
+
+
+        /// Retourner un booléen selon le nombre d'oeuvres trouvé
+        if(nbOeuvre==0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
